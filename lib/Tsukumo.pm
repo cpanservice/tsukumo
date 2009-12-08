c9424bf173e3766315f25f6cff221083cd80e4e0
nyarla <thotep@nyarla.net>
2009-12-08 17:23:42 +0900

    improt module-setup template

---
+++ lib/Tsukumo.pm
@@ -0,0 +1,33 @@
+package Tsukumo;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Tsukumo -
+
+=head1 SYNOPSIS
+
+  use Tsukumo;
+
+=head1 DESCRIPTION
+
+Tsukumo is
+
+=head1 AUTHOR
+
+Naoki Okamrua (Nyarla) E<lt>nyarla[ at ]thotep.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
