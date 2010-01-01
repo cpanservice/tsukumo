981755b88dc241dfedf45bb629fab269fe14b494
nyarla <thotep@nyarla.net>
2010-01-01 15:39:09 +0900

    Tsukumo::Role: add class loader.

--- lib/Tsukumo/Role.pm
+++ lib/Tsukumo/Role.pm
@@ -19,31 +19,49 @@ sub init_role {
 }
 
 sub end_of_role {
-    my ( $target ) = @_;
+    my ( $target, $unimport, @args ) = @_;
 
     my $class = any_moose('::Role');
 
-    eval qq{package ${target}; ${class}->unimport()};
+    my $eval = qq{package ${target};\n};
+    for my $module ( @{ $unimport } ) {
+        $eval .= qq{ ${module}->unimport(); \n};
+    }
+      $eval .= qq{${class}->unimport};
+
+    local $@;
+    eval $eval;
+    Tsukumo::Exception->throw( error => "Cannot unimport Tsukumo::Role: $@" )
+        if ( $@ );
 
     return 1;
 }
 
 sub install_end_of_role {
-    my ( $target ) = @_;
+    my ( $target, $unimport ) = @_;
 
     no strict 'refs';
     *{"${target}::__END_OF_ROLE__"} = sub {
         my $caller = caller(0);
-        end_of_role($caller);
+        end_of_role($caller, $unimport, @_);
     };
 }
 
 
 sub import {
-    my $class   = shift;
-    my $caller  = caller(0);
+    my $class       = shift;
+    my $caller      = caller(0);
+    my @modules     = ();
+    my @unimport    = ();
 
-    install_end_of_role($caller);
+    while ( my $module = shift @_ ) {
+        my $args = @_ && ref($_[0]) ? shift : [];
+        $module = any_moose($module);
+        push @modules, ( $module, $args );
+        push @unimport, $module;
+    }
+
+    install_end_of_role($caller, \@unimport);
 
     strict->import;
     warnings->import;
@@ -51,14 +69,24 @@ sub import {
 
     init_role($caller);
 
-    
-
     if ( Any::Moose::moose_is_preferred() ) {
         Moose::Role->import({ into_level => 1 });
     }
     else {
         Mouse::Role->export_to_level(1);
     }
+
+    while ( my ( $module, $args ) = splice @modules, 0, 2 ) {
+        local $@;
+        eval { Any::Moose::load_class($module) };
+        Tsukumo::Exception->throw( error => "Cannot import Tsukumo::Role: $@" ) if ( $@ );
+
+        eval qq{package ${caller};\n}
+            . q{$module->import( @{ $args } )};
+        Tsukumo::Exception->throw( error => "Cannot import Tsukumo::Role: $@" ) if ( $@ );
+    }
+
+    return;
 }
 
 no Any::Moose;
--- t/00_core/011_role/20_use.t
+++ t/00_core/011_role/20_use.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 3;
 
 {
     package TestRole;
@@ -17,3 +17,16 @@ use Test::More tests => 2;
 can_ok( 'TestRole', qw( meta __END_OF_ROLE__ ) );
 
 ok( ! TestRole->can('has') );
+
+{
+    package TestRoleWithModule;
+    
+    use strict;
+    use Tsukumo::Role 'X::AttributeHelpers';
+
+    __END_OF_ROLE__;
+}
+
+my $class = ( Any::Moose::moose_is_preferred() ) ? 'Moose' : 'Mouse' ;
+
+ok( Any::Moose::is_class_loaded("${class}X::AttributeHelpers") );
